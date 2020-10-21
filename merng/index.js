3ebaddf01e2da93bda225ba6041323145aaa6ab2
Abrar Akhand <Jahin345@gmail.com>
2020-10-20 23:04:00 -0700

    Connected to MongoDB database

--- merng/index.js
+++ merng/index.js
@@ -1,6 +1,11 @@
 const { ApolloServer } = require('apollo-server');
 const gql = require('graphql-tag');
 
+// for connecting to database, 
+// allows us to innterface with the mongoDB database
+const mongoose = require('mongoose');   
+
+const { MONGODB } = require('./config.js');
 
 const typeDefs =  gql`
     type Query{
@@ -19,11 +24,20 @@ const server = new ApolloServer({
     resolvers
 });
 
-// creating server at port 5000
-server.listen({port: 5000})
-    .then(res => {
+
+// finding the server port
+// if no website port found, then create using port = 3000
+// localhost:3000
+const port = process.env.PORT || 3000;
+
+mongoose.connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        console.log('MongoDB has connected!');
+        server.listen(port)
+    })
+    .then((res) => {
         // outputs the link to the website/server in the terminal
-        console.log(`Server Running at ${res.url}`)
+        console.log(`Server Running at ${port}`);
         // type "node index" to start the server
         // press CTRL + C to stop the server
-    })
+    });
